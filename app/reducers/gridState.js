495cf052f92510152542e72b9fc1a89b967442ba
Ian Fisk <i.fisk32@gmail.com>
2016-09-28 23:44:23 -0700

    Update gridState reducer

--- app/reducers/gridState.js
+++ app/reducers/gridState.js
@@ -1,16 +1,26 @@
 'use strict';
 
-import { GRID_CELL_CLICKED, PRELOAD_DATA } from '../actions';
+import { GRID_CELL_CLICKED, USER_CHARACTER_CHOSEN, RESET_GAME } from '../actions';
+import { convertTo2DIndex, deepCopy2DArray } from '../utility/generalUtility';
 
-const initialGridState = ['', '', '', '', '', '', '', '', ''];
+const initialGridState = [['', '', ''],
+  ['', '', ''],
+  ['', '', '']];
 
 const gridState = (state = initialGridState, action) => {
   switch (action.type) {
     case GRID_CELL_CLICKED:
-      let newGridState = state.slice();
-      newGridState[action.cellIndex] = action.userCharacter == 'X' ? 'X' : 'O';
-      return newGridState;
-      
+      let twoDimensionalIndex = convertTo2DIndex(action.cellIndex, 3);
+
+      if (state[twoDimensionalIndex.row][twoDimensionalIndex.column] === '') {
+        let newGridState = deepCopy2DArray(state);
+        newGridState[twoDimensionalIndex.row][twoDimensionalIndex.column] = action.userCharacter === 'X' ? 'X' : 'O';          
+        
+        return newGridState;
+      }
+    case USER_CHARACTER_CHOSEN:
+    case RESET_GAME:
+      return initialGridState;
     default:
       return state;
   }
